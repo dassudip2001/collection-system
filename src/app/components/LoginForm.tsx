c48c675624fa1c0f1c7f996f8a0a878262d0493b
Sudip Das <sudip5428@gmail.com>
2024-12-14 21:52:29 +0530

    login form ui changes

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import Link from "next/link";
-import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
@@ -34,33 +35,31 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="grid place-items-center h-screen">
-      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+    <div className="grid place-items-center h-screen ">
+      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400 w-1/4 h-1/3">
         <h1 className="text-xl font-bold my-4">Login</h1>
 
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-          <input
+          <Input
             onChange={(e) => setEmail(e.target.value)}
             type="text"
             placeholder="Email"
           />
-          <input
+          <Input
             onChange={(e) => setPassword(e.target.value)}
             type="password"
             placeholder="Password"
           />
-          <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
+          <Button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
             Login
-          </button>
+          </Button>
           {error && (
             <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
               {error}
             </div>
           )}
 
-          <Link className="text-sm mt-3 text-right" href={"/register"}>
-            Don&apos;t have an account? <span className="underline">Register</span>
-          </Link>
+          
         </form>
       </div>
     </div>
