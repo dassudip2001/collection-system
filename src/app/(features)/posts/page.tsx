18285b86ecfeea3fba9e538d742f8a19ff8d4025
Sudip Das <sudip5428@gmail.com>
2024-12-22 11:52:56 +0530

    code commit for navbar design chanhes

---
+++ src/app/(features)/posts/page.tsx
@@ -0,0 +1,7 @@
+export default function Posts() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Posts</h1>
+    </div>
+  );
+}
--- src/app/components/category/CategoryForm.tsx
+++ src/app/components/category/CategoryForm.tsx
@@ -130,7 +130,7 @@ export default function CategoryForm() {
     <div>
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div className="bg-gray-200 overflow-hidden shadow-sm sm:rounded-lg">
+          <div className="bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 text-gray-900">
               <div className="flex justify-between">
                 <h2 className="text-2xl font-semibold">Category</h2>
